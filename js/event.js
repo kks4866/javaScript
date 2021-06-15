1e29aef05dec9050105427a5c6db148cfdb31763
kks4866@naver.com <kks4866@naver.com>
2021-06-15 09:35:53 +0900

    자바 스크립트

---
+++ js/event.js
@@ -0,0 +1,10 @@
+//상세 설명 보기 클릭 -텍스트 보이기
+function showText(){
+   var text = document.getElementById("desc");
+   text.style.display = "block"; //수직으로 보이기
+}
+
+function hideText(){
+    var text = document.getElementById("desc");
+   text.style.display = "none";
+}
